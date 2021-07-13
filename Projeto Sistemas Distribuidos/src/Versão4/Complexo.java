ee5f8e469f769a9a31bf8dda44bc8fdb36a9dcfc
guilherme <insanogamer20@gmail.com>
2021-07-13 20:56:23 -0300

    versão4

---
+++ Projeto Sistemas Distribuidos/src/Versão4/Complexo.java
@@ -0,0 +1,14 @@
+package Versão4;
+
+import java.io.*;
+
+public class Complexo implements Serializable {
+	double a, b;
+
+	public Complexo() {
+	}
+
+	public double modulo() {
+		return Math.sqrt(a * a + b * b);
+	}
+}
